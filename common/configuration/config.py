9f40b955a6109fbe9875320e293729c33d9b0dc4
Luke Beckwith <luke98@vt.edu>
2018-08-08 18:30:03 -0400

    cleaning

--- common/configuration/config.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def get_cfg():
-    cfg_file = os.path.dirname(os.path.realpath(__file__))+'\config.json'
-    with open(cfg_file) as f:
-        data = json.load(f)
-    return data
